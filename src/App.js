d9f6c39b847e14a71b4720d7a77110f0791a669e
leduchao <leduchao77g1@gmail.com>
2023-06-29 00:52:01 +0700

    initial todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+
+      <input type='text' className='enter-job' placeholder='Add your new todo' />
+      <input type='button' className='add-job' value={'ADD'} />
+
+      <input type='button' className='job' value={'Buy a new laptop'} />
+      <input type='button' className='job' value={'Buy a new laptop'} />
+      <input type='button' className='job' value={'Buy a new laptop'} />
+      <input type='button' className='job' value={'Buy a new laptop'} />
+      <input type='button' className='job' value={'Buy a new laptop'} />
+      
+      <div className='footer'>
+        <p>You have <span>5</span> pending tasks</p>
+        <input type='button' className='clear-all' value={'CLEAR ALL'} />
+      </div>
     </div>
   );
 }
