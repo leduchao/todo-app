e8572aab7b50f844485c6c6510c3d92981827f6a
leduchao <leduchao77g1@gmail.com>
2023-06-29 17:36:45 +0700

    add to local storage and delete job

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const storage = JSON.parse(localStorage.getItem('jobs'));
+
+  const [job, setJob] = useState('');
+
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs')) ?? [];
+    return storageJobs;
+  });
+
+  const [isDone, setIsDone] = useState(true);
+
+  const addJob = () => {
+    if (!job) {
+      alert('Enter an item!');
+    } else {
+      setJobs(prev => {
+        const newJobs = [...prev, job]
+  
+        const jsonJobs = JSON.stringify(newJobs);
+        localStorage.setItem('jobs', jsonJobs);
+  
+        return newJobs;
+      });
+      setJob('');
+    }
+
+  }
+
+  const clearAllJob = () => {
+    localStorage.clear();
+    setJobs([]);
+  }
+
+  const jobFinished = (e) => {
+    setIsDone(done => !done);
+
+    e.target.style.color = isDone ? 'silver' : 'black';
+  }
+
+  const deleteJob = (index) => {
+    const storage = JSON.parse(localStorage.getItem('jobs'))
+
+    storage.splice(index, 1);
+
+    localStorage.setItem('jobs', JSON.stringify(storage));
+    const newStorage = JSON.parse(localStorage.getItem('jobs')) ?? [];
+    
+    setJobs(newStorage);
+  }
+
   return (
     <div className="App">
       <h1>Todo App</h1>
 
-      <input type='text' className='enter-job' placeholder='Add your new todo' />
-      <input type='button' className='add-job' value={'ADD'} />
+      <input type='text' className='enter-job' placeholder='Add your new todo'
+        value={job} onChange={e => setJob(e.target.value)} />
+      <input type='submit' className='add-job' value={'ADD'} onClick={addJob} />
 
-      <input type='button' className='job' value={'Buy a new laptop'} />
-      <input type='button' className='job' value={'Buy a new laptop'} />
-      <input type='button' className='job' value={'Buy a new laptop'} />
-      <input type='button' className='job' value={'Buy a new laptop'} />
-      <input type='button' className='job' value={'Buy a new laptop'} />
+      {jobs.map((job, index) => (
+        <div className='list-jobs'>
+          <input type='button' className='job' key={index} value={job}
+            onClick={(e) => jobFinished(e)}/>
+          {/* <input type='checkbox' className='job-action done-btn' value={'DONE'}/> */}
+          <input type='submit' className='job-action delete-btn' value={'DELETE'}
+            onClick={() => deleteJob(index)}/>
+        </div>
+      ))}
       
       <div className='footer'>
-        <p>You have <span>5</span> pending tasks</p>
-        <input type='button' className='clear-all' value={'CLEAR ALL'} />
+        <p>You have <span>{jobs.length}</span> pending tasks</p>
+        <input type='submit' className='clear-all' value={'CLEAR ALL'}
+          onClick={clearAllJob} />
       </div>
     </div>
   );
